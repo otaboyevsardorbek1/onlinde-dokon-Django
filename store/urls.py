890380970f36b6865799057282559321e8543d92
khasanovmma <khasanovmma010@gmail.com>
2022-06-25 20:20:57 +0500

    refactor code

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,6 @@ from .views import home_page_view
 urlpatterns = [
     path("", home_page_view, name="home"),
     path("shop/", home_page_view, name="shop"),
+    path("shop/", home_page_view, name="shop"),
+    # path("shop/", home_page_view, name="shop"),
 ]
