3ef20312f6ad51b201e3736db89aa3591375a4b6
muhammadjon-dev <muhammadjona2004@gmail.com>
2022-06-25 20:25:53 +0500

    fix code

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,4 @@ from .views import home_page_view
 urlpatterns = [
     path("", home_page_view, name="home"),
     path("shop/", home_page_view, name="shop"),
-    path("shop/", home_page_view, name="shop"),
-    # path("shop/", home_page_view, name="shop"),
 ]
